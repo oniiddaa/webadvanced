fca998b8928c5d2c8f49d003060f097b6ff6aa6d
oniiddaa <onidamisiri1112@gmail.com>
2026-03-24 19:19:54 +0100

    modul26

---
+++ Module 26/main.js
@@ -0,0 +1,58 @@
+console.log("g");
+
+
+var x=5 *4727;
+
+console.log(x);
+
+var a=40;
+var b=10
+var c=a - b;
+console.log(c);
+
+var h=40;
+var i=10
+var j=h + i;
+console.log(h);
+
+// var m=40;
+// var n=10
+// var o=m / n;
+// console.log(o);
+
+// var p=40;
+// var q=10
+// var r=p % q;
+// console.log(r);
+
+var n=99;
+n++;
+console.log(n);
+
+var k=99;
+k--;
+console.log(k);
+
+var p=20;
+var q= p ** 2;
+console.log(q);
+
+var y=15;
+y+=5;
+// y-=5;
+// y*=5;
+// y/=5;
+console.log(y);
+
+var b1=5;
+var b6="88";
+
+console.log(b1>b6);
+console.log(b1<b6);
+console.log(b1==b6);
+
+var name="filan";
+var surname="fisteku";
+
+console.log(name+surname);
+
