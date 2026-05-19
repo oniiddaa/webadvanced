9f131f1fb563b1400d2c4b2a03d6001eecf47888
oniiddaa <onidamisiri1112@gmail.com>
2026-05-19 19:14:44 +0200

    modul38

---
+++ Module 38/main.js
@@ -0,0 +1,23 @@
+$(function(){
+    alert("Hello")
+});
+
+$('#sort').sortable();
+
+$(function(){
+    $('#accordion').accordion();
+});
+
+$(function(){
+    $('#accordion').accordion({
+        collapsible: true
+    });
+});
+
+$('#tabs').tabs();
+
+$(function(){
+    $('#tabs').tabs({
+        collapsible: true
+    });
+});
